ad3ef871d37ee5933e9a74d4cc06cd8a1b57bc69
brinderdeep1997 <58747840+brinderdeep1997@users.noreply.github.com>
2019-12-10 16:10:53 -0500

    Add files via upload

---
+++ src/categories.java
@@ -0,0 +1,11 @@
+
+public class categories {
+String categoryname;
+String description;
+String path;
+categories(String categoryname,String description,String path){
+    this.categoryname=categoryname;
+    this.description=description;
+    this.path=path;
+}
+}
